460a8491baffed47df1b0ca5b60720ae683a6f6a
megarohas <a.galkin@desoft.pro>
2021-08-12 12:11:56 +0600

    string_includes_one_of

---
+++ index.js
@@ -0,0 +1,10 @@
+function string_includes_one_of(_str, _arr) {
+  let result = false;
+  for (let i = 0; i < _arr.length; i++) {
+    if (_str.includes(_arr[i])) {
+      result = true;
+      break;
+    }
+  }
+  return result;
+}
