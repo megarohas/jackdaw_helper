9d6f93060b5930df1e0e258a2076f616207ec0d5
megarohas <a.galkin@desoft.pro>
2021-08-12 15:47:34 +0600

    version changes

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // import * as Other from "./utils/other.js";
 // import * as Request from "./utils/request.js";
+
 const Other = require("./utils/other.js");
 const Request = require("./utils/request.js");
+//
+// exports.Other = Other;
+// exports.Request = Request;
 
-exports.Other = Other;
-exports.Request = Request;
+export function UseOther() {
+  return Other;
+}
+export function UseRequest() {
+  return Request;
+}
