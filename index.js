7d41538c7fbc95028ba8361b5ef678d64af62896
megarohas <a.galkin@desoft.pro>
2021-08-12 12:12:31 +0600

    string_includes_one_of changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function string_includes_one_of(_str, _arr) {
   let result = false;
   for (let i = 0; i < _arr.length; i++) {
-    if (_str.includes(_arr[i])) {
+    if (_str.includes(_arr[i].toString())) {
       result = true;
       break;
     }
