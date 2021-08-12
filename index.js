98d32f0943e4002f6fe1a275f25d07de0d3eaf32
megarohas <a.galkin@desoft.pro>
2021-08-12 15:07:28 +0600

    structure changes

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-import * as Other from "./utils/other.js";
-import * as Request from "./utils/request.js";
+// import * as Other from "./utils/other.js";
+// import * as Request from "./utils/request.js";
+require("./utils/other.js");
--- utils/other.js
+++ utils/other.js
@@ -1,17 +1,19 @@
-export function stringIncludesOneOfArray(_str, _arr) {
-  let result = false;
-  for (let i = 0; i < _arr.length; i++) {
-    if (_str.includes(_arr[i].toString())) {
-      result = true;
-      break;
+module.exports = class Other {
+  stringIncludesOneOfArray(_str, _arr) {
+    let result = false;
+    for (let i = 0; i < _arr.length; i++) {
+      if (_str.includes(_arr[i].toString())) {
+        result = true;
+        break;
+      }
     }
+    return result;
   }
-  return result;
-}
 
-export function excludeDuplicatesFromArray(array, field) {
-  return array.filter(
-    (obj, index, self) =>
-      index === self.findIndex((el) => el[field] === obj[field])
-  );
-}
+  excludeDuplicatesFromArray(array, field) {
+    return array.filter(
+      (obj, index, self) =>
+        index === self.findIndex((el) => el[field] === obj[field])
+    );
+  }
+};
